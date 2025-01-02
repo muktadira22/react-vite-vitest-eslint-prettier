07d46032b10be5a2bfdbda2d96cbfaa3f31434de
Muhammad Aria Muktadir <muhammad.aria@dansmultipro.com>
2025-01-02 14:20:35 +0700

    Update Eslint Settings

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,30 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
 
 export default tseslint
   .config(
     { ignores: ["dist"] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...tseslint.configs.stylisticTypeChecked,
+      ],
       files: ["**/*.{ts,tsx}"],
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
       plugins: {
         "react-hooks": reactHooks,
         "react-refresh": reactRefresh,
+        react,
       },
       rules: {
         ...reactHooks.configs.recommended.rules,
@@ -25,6 +35,14 @@ export default tseslint
           "warn",
           { allowConstantExport: true },
         ],
+        // Enable its recommended rules
+        ...react.configs.recommended.rules,
+        ...react.configs["jsx-runtime"].rules,
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
       },
     },
   )
